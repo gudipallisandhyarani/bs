a1565279a6c2c100e7b043abefce6184f4f2a715
gudipallisandhyarani <gudipallisandhyarani239@gmail.com>
2023-11-06 05:07:02 +0000

    And

---
+++ src/components/Home/index.js
@@ -0,0 +1,11 @@
+import BlogsList from '../BlogsList'
+import UserInfo from '../UserInfo'
+
+const Home = () => (
+  <div className="home-container">
+    <UserInfo />
+    <BlogsList />
+  </div>
+)
+
+export default Home
